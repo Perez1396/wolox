af090e039bcf5102f22f14823705d3d435ee8b44
Juan Daniel <juan.perez@utp.edu.co>
2021-04-22 14:46:14 -0500

    Creation of the data.sql file to initialize the DB with default data

---
+++ wolox/src/main/resources/data.sql
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Usuario
+ * Created: 22/04/2021
+ */
+
+
+INSERT INTO permissions (id, user_id, album_id) VALUES
+(1, 1, 1),
+(2, 1, 2);
+
+INSERT INTO permissions_types(permissions_id, types) VALUES
+(1,'READ'),
+(2,'READ');
